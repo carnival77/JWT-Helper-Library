e0856dcffe8a63520df845be851d8e0c29f2b7c0
Carnival77 <stam0325@gmail.com>
2024-12-13 15:05:38 +0900

    refactor : 단축URL 생성 시 URL 유효성 검사 조건 보완

--- examples/shortUrl/src/main/java/com/example/shortUrl/domain/dto/ShortenUrlCreateRequestDto.java
+++ examples/shortUrl/src/main/java/com/example/shortUrl/domain/dto/ShortenUrlCreateRequestDto.java
@@ -10,6 +10,6 @@ import org.hibernate.validator.constraints.URL;
 public class ShortenUrlCreateRequestDto {
 
     @NotNull
-    @URL(regexp = "^(https?|ftp):\\/\\/www\\.[A-Za-z0-9\\-._~:/?#\\[\\]@!$&'()*+,;=]+$")
+    @URL(regexp = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")
     private String originalUrl;
 }
